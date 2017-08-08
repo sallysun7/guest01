2686cd145f18aadfd0daa66a5647b448a96a3e43
sallysun7 <776760591@qq.com>
2017-08-08 18:00:23 +0800

    88
    88

--- sign/views.py
+++ sign/views.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
-# Create your views here.
-def index(request):
-    return render(request,"index.html")
+from django.http import HttpResponse, HttpResponseRedirect
 
 
+#def index(request):
+   # return render(request, 'index.html')
+
+
+# 登录
 def login_action(request):
     if request.method == 'POST':
-        username = request.POST.get('username','')
-        password = request.POST.get('password','')
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
         if username == 'admin' and password == 'admin123':
-            return HttpResponse('success!')
-            #return HttpResponseRedirect('/event_manage/')
+            response = HttpResponseRedirect('/event_manage/')
+            # response.set_cookie('user',username,3600) #添加浏览器cookie
+            assert isinstance(username, object)
+            request.session['user'] = username  # session信息记录到浏览器
+            return response
         else:
-            return render(request,'index.html',{'error':'login error'})
-#def event_manage(request):
-    #return render(request,"event_manage.html")
+
+
+# 发布会管理
+def event_manage(request):
+    #username = request.COOKIES.get('user', '')  # 读取浏览器cookie
+    username = request.session.get('user', '')  # 读取浏览器session
+    return render(request, "event_manage.html", {"user": username})
